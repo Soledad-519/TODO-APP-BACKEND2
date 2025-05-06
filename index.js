3bcfe024a96a86df7fcaa997b658cb40a99d24e8
Svarium <you@example.com>
2025-05-05 21:31:28 -0300

    index config in progress

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 import express from "express"; //importar express
+import dotenv from "dotenv"; //importar dotenv
+import fs from "node:fs"; //importar fs
 
+dotenv.config() //ejecuta dotenv
 
 const app = express(); //crear una instancia de express
 
-
-const PORT = 3001; //definir el puerto
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
     console.log("Server is running on port: http://localhost:"+PORT); //iniciar el servidor en el puerto 3000
 })
+
+//MANEJO DE RUTAS
---
+++ src/routes/usersRoutes.js
@@ -0,0 +1,13 @@
+import express from 'express'; //importo express
+
+const router = express.Router(); //creo una instancia de express.Router()
+
+router.get("/", (req,res) => {
+    res.send("Esta es la ruta para los usuarios")
+})
+
+router.get("/about", (req,res) => {
+    res.send("Esta es la ruta de about")
+})
+
+export default router; //exporto la instancia de express.Router()
