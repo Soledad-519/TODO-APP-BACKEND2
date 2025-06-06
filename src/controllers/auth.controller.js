ad0a397ce824657337d6ace39329fc24a092bec1
soledad <mariasoledadcaria@gmail.com>
2025-06-06 18:36:19 -0300

    cambios  y creacion de nuevas carpetas

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,32 +1,11 @@
-export const home = (req,res) => {
-    res.send("Esta es la ruta home para los usuarios") // mensaje de benvenida
-}
+import User from '../models/user.model.js';
+import bcrypt from "bcryptjs"
 
-export const about = (req, res) => {
-    res.send("Esta es la ruta de about")
-}
 
-export const getUser = (req,res) => {
-    try {
-      const {username, email, edad, password} = req.body; //obtengo el usuario del body de la peticion
 
-      if(!username || !email || !edad || !password){
-        return res.status(400).json({message:"Todos los datos del usuario son obligatorios"}) //si no se envia el usuario, devuelvo un error 400
-      }     
 
-      console.log(req.body);
-      
-      return res.status(200).json({
-        user: {
-            username,
-            email,
-            edad,
-            password
-        }, //devuelvo el usuario
-        message: "Usuario obtenido correctamente" //mensaje de exito 
-      })         
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({message:"Error obteniendo el usuario"})        
-    }
+export const register = async(req, res) => {
+
+  
+
 }
--- src/routes/usersRoutes.js
+++ src/routes/usersRoutes.js
@@ -1,12 +1,12 @@
 import express from 'express'; //importo express
-import { about, getUser, home } from '../controllers/auth.controller.js';
+import { validateSchema } from '../middlewares/validator.middleware.js';
+import { registerSchema } from '../validators/auth.validator.js';
+import {register} from '../controllers/auth.controller.js';
+
 
 const router = express.Router(); //creo una instancia de express.Router()
 
-router.get("/", home);
+router.post('/register', validateSchema(registerSchema), register)
 
-router.get("/about", about);
-
-router.post("/getUser", getUser); //ruta para obtener el usuario
 
 export default router; //exporto la instancia de express.Router()
